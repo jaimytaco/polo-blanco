24f8d51c45bbb6437ebd18159f2a43070062b208
jaimytaco <jaimy.taco@gmail.com>
2021-12-30 11:45:29 -0500

    [UPD] throw err in astro-build

--- src/scripts/services/firebase.service.ts
+++ src/scripts/services/firebase.service.ts
@@ -4,6 +4,7 @@ import { isBrowser, isWorker, isNode } from '../helpers/browser.helper'
 import { initializeApp as initializeApp_JS } from '../libs/firebase-app-JS'
 import { getFirestore as getFirestore_JS, collection as collection_JS, getDocs as getDocs_JS } from '../libs/firebase-firestore-JS'
 
+
 export class Firebase {
     static app: any
     static firestore: any
@@ -26,44 +27,11 @@ export class Firebase {
     static collection: Function
     static getDocs: Function
 
-    // static async init() {
-    //     this.FIREBASE_APP_JS = `https://www.gstatic.com/firebasejs/${this.FIREBASE_VERSION}/firebase-app.js`
-    //     this.FIREBASE_FIRESTORE_JS = `https://www.gstatic.com/firebasejs/${this.FIREBASE_VERSION}/firebase-firestore.js`
-
-    //     const { initializeApp } = await import(isBrowser() || isWorker() ? this.FIREBASE_APP_JS : this.FIREBASE_APP_NODE)
-    //     const { getFirestore } = await import(isBrowser() || isWorker() ? this.FIREBASE_FIRESTORE_JS : this.FIREBASE_FIRESTORE_NODE)
-
-    //     this.app = initializeApp(this.FIREBASE_CONFIG)
-    //     this.firestore = getFirestore(this.app)
-    // }
-
-    // static async getAll(collectionName: string): Promise<any[] | IError> {
-    //     const { collection, getDocs } = await import(isBrowser() || isWorker() ? this.FIREBASE_FIRESTORE_JS : this.FIREBASE_FIRESTORE_NODE)
-
-    //     try {
-    //         const q = collection(this.firestore, collectionName)
-    //         const snapshot = await getDocs(q)
-    //         const docs = snapshot.docs
-    //             .map(function (doc) {
-    //                 const data = doc.data()
-    //                 data.id = doc.id
-    //                 return data
-    //             })
-    //         return docs
-    //     } catch (err) {
-    //         return { err }
-    //     }
-    // }
-
     static async init() {
         this.FIREBASE_APP_JS = `https://www.gstatic.com/firebasejs/${this.FIREBASE_VERSION}/firebase-app.js`
         this.FIREBASE_FIRESTORE_JS = `https://www.gstatic.com/firebasejs/${this.FIREBASE_VERSION}/firebase-firestore.js`
 
         if (isNode()){
-            // const { initializeApp } = await import(isBrowser() || isWorker() ? this.FIREBASE_APP_JS : this.FIREBASE_APP_NODE)
-            // const { getFirestore } = await import(isBrowser() || isWorker() ? this.FIREBASE_FIRESTORE_JS : this.FIREBASE_FIRESTORE_NODE)
-            // const { collection, getDocs } = await import(isBrowser() || isWorker() ? this.FIREBASE_FIRESTORE_JS : this.FIREBASE_FIRESTORE_NODE)
-            
             const { initializeApp } = await import(this.FIREBASE_APP_NODE)
             const { getFirestore, collection, getDocs } = await import(this.FIREBASE_FIRESTORE_NODE)
 
@@ -83,8 +51,6 @@ export class Firebase {
     }
 
     static async getAll(collectionName: string): Promise<any[] | IError> {
-        // const { collection, getDocs } = await import(isBrowser() || isWorker() ? this.FIREBASE_FIRESTORE_JS : this.FIREBASE_FIRESTORE_NODE)
-
         try {
             const q = this.collection(this.firestore, collectionName)
             const snapshot = await this.getDocs(q)
